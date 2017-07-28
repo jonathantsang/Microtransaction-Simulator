36cbc7aa26caf44b01787e175b3190434f8d17fe
jonathantsang <j26tsang@gmail.com>
2017-07-28 01:55:36 -0400

    first commit

---
+++ Assets/_scripts/logic/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	[Header("GameData")]
+	private int money;
+	private int crates;
+	private int keys;
+
+	public static GameController instance;
+
+	// Use this for initialization
+	void Start () {
+		// Singleton Behaviour
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject);    
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_scripts/logic/gameUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameUI : MonoBehaviour {
+
+	private GameObject banner;
+
+	private Text wallet;
+	private Text crates;
+	private Text keys;
+
+	// Use this for initialization
+	void Start () {
+		banner = GameObject.FindGameObjectWithTag ("banner");
+		if (banner != null) {
+			wallet = banner.transform.GetChild (0).GetComponent<Text> ();
+			crates = banner.transform.GetChild (1).GetComponent<Text> ();
+			keys = banner.transform.GetChild (2).GetComponent<Text> ();
+			wallet.text = "Wallet: $ " + 0;
+			crates.text = "Crates: " + 0;
+			keys.text = "Keys: " + 0;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
