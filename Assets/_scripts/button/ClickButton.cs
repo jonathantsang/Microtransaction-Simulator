eb373251b917ef578589356ff667209c8eddf6ae
jonathantsang <j26tsang@gmail.com>
2017-08-06 01:10:49 -0400

    open page start

---
+++ Assets/_scripts/button/ClickButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	virtual public void buttonEffects(){
+	}
+
+	void OnMouseDown(){
+		buttonEffects ();
+	}
+}
---
+++ Assets/_scripts/button/OpenButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenButton : ClickButton {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	override public void buttonEffects(){
+		print ("open");
+	}
+}
